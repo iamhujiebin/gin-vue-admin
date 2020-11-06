ce20ef11d122579047005d294fd4011538f9a38d
iamhujiebin <458249864@qq.com>
2020-11-06 14:27:05 +0800

    feat：优化了一下上传的展示下载路径。

--- web/src/view/example/upload/upload.vue
+++ web/src/view/example/upload/upload.vue
@@ -4,12 +4,12 @@
       <el-row>
         <el-col :span="12">
           <el-upload
-            :action="`${path}/fileUploadAndDownload/upload`"
-            :before-upload="checkFile"
-            :headers="{ 'x-token': token }"
-            :on-error="uploadError"
-            :on-success="uploadSuccess"
-            :show-file-list="false"
+              :action="`${path}/fileUploadAndDownload/upload`"
+              :before-upload="checkFile"
+              :headers="{ 'x-token': token }"
+              :on-error="uploadError"
+              :on-success="uploadSuccess"
+              :show-file-list="false"
           >
             <el-button size="small" type="primary">点击上传</el-button>
             <div class="el-upload__tip" slot="tip">
@@ -19,7 +19,7 @@
         </el-col>
         <el-col :span="12">
           带压缩的上传, (512(k)为压缩限制)
-          <upload-image v-model="imageUrl" :fileSize="512" :maxWH="1080" />
+          <upload-image v-model="imageUrl" :fileSize="512" :maxWH="1080"/>
           已上传文件 {{ imageUrl }}
         </el-col>
       </el-row>
@@ -27,7 +27,7 @@
       <el-table :data="tableData" border stripe>
         <el-table-column label="预览" width="100">
           <template slot-scope="scope">
-            <CustomPic picType="file" :picSrc="scope.row.url" />
+            <CustomPic picType="file" :picSrc="scope.row.url"/>
           </template>
         </el-table-column>
         <el-table-column label="日期" prop="UpdatedAt" width="180">
@@ -36,44 +36,48 @@
           </template>
         </el-table-column>
         <el-table-column
-          label="文件名"
-          prop="name"
-          width="180"
+            label="文件名"
+            prop="name"
+            width="180"
         ></el-table-column>
         <el-table-column
-          label="链接"
-          prop="url"
-          min-width="300"
-        ></el-table-column>
+            label="链接"
+            min-width="300"
+        >
+          <template slot-scope="scope">{{ domain + path + scope.row.url }}</template>
+        </el-table-column>
         <el-table-column label="标签" prop="tag" width="100">
           <template slot-scope="scope">
             <el-tag
-              :type="scope.row.tag === 'jpg' ? 'primary' : 'success'"
-              disable-transitions
-              >{{ scope.row.tag }}</el-tag
+                :type="scope.row.tag === 'jpg' ? 'primary' : 'success'"
+                disable-transitions
+            >{{ scope.row.tag }}
+            </el-tag
             >
           </template>
         </el-table-column>
         <el-table-column label="操作" width="160">
           <template slot-scope="scope">
             <el-button @click="downloadFile(scope.row)" size="small" type="text"
-              >下载</el-button
+            >下载
+            </el-button
             >
             <el-button @click="deleteFile(scope.row)" size="small" type="text"
-              >删除</el-button
+            >删除
+            </el-button
             >
           </template>
         </el-table-column>
       </el-table>
       <el-pagination
-        :current-page="page"
-        :page-size="pageSize"
-        :page-sizes="[10, 30, 50, 100]"
-        :style="{ float: 'right', padding: '20px' }"
-        :total="total"
-        @current-change="handleCurrentChange"
-        @size-change="handleSizeChange"
-        layout="total, sizes, prev, pager, next, jumper"
+          :current-page="page"
+          :page-size="pageSize"
+          :page-sizes="[10, 30, 50, 100]"
+          :style="{ float: 'right', padding: '20px' }"
+          :total="total"
+          @current-change="handleCurrentChange"
+          @size-change="handleSizeChange"
+          layout="total, sizes, prev, pager, next, jumper"
       ></el-pagination>
     </div>
   </div>
@@ -81,13 +85,15 @@
 
 <script>
 const path = process.env.VUE_APP_BASE_API;
-import { mapGetters } from "vuex";
+const domain = process.env["VUE_APP_DOMAIN"];
+import {mapGetters} from "vuex";
 import infoList from "@/mixins/infoList";
-import { getFileList, deleteFile } from "@/api/fileUploadAndDownload";
-import { downloadImage } from "@/utils/downloadImg";
-import { formatTimeToStr } from "@/utils/date";
+import {getFileList, deleteFile} from "@/api/fileUploadAndDownload";
+import {downloadImage} from "@/utils/downloadImg";
+import {formatTimeToStr} from "@/utils/date";
 import CustomPic from "@/components/customPic";
 import UploadImage from "@/components/upload/image.vue";
+
 export default {
   name: "Upload",
   mixins: [infoList],
@@ -100,6 +106,7 @@ export default {
       fullscreenLoading: false,
       listApi: getFileList,
       path: path,
+      domain: domain,
       tableData: [],
       imageUrl: "",
     };
@@ -124,22 +131,22 @@ export default {
         cancelButtonText: "取消",
         type: "warning",
       })
-        .then(async () => {
-          const res = await deleteFile(row);
-          if (res.code == 0) {
+          .then(async () => {
+            const res = await deleteFile(row);
+            if (res.code == 0) {
+              this.$message({
+                type: "success",
+                message: "删除成功!",
+              });
+              this.getTableData();
+            }
+          })
+          .catch(() => {
             this.$message({
-              type: "success",
-              message: "删除成功!",
+              type: "info",
+              message: "已取消删除",
             });
-            this.getTableData();
-          }
-        })
-        .catch(() => {
-          this.$message({
-            type: "info",
-            message: "已取消删除",
           });
-        });
     },
     checkFile(file) {
       this.fullscreenLoading = true;
@@ -181,7 +188,8 @@ export default {
       this.fullscreenLoading = false;
     },
     downloadFile(row) {
-      downloadImage(row.url, row.name);
+      const url = domain + path + row.url
+      downloadImage(url, row.name);
     },
   },
   created() {
