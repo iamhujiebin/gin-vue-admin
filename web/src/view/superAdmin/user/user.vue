bb4f2889bf18d3254707837966b530c08b94d47a
iamhujiebin <458249864@qq.com>
2020-11-20 01:20:16 +0800

    fix：新建用户获取头像的bug

--- web/src/view/superAdmin/user/user.vue
+++ web/src/view/superAdmin/user/user.vue
@@ -7,7 +7,7 @@
       <el-table-column label="头像" min-width="50">
         <template slot-scope="scope">
           <div :style="{'textAlign':'center'}">
-            <CustomPic :picSrc="scope.row.headerImg" />
+            <CustomPic :picSrc="scope.row.headerImg"/>
           </div>
         </template>
       </el-table-column>
@@ -17,12 +17,12 @@
       <el-table-column label="用户角色" min-width="150">
         <template slot-scope="scope">
           <el-cascader
-            @change="changeAuthority(scope.row)"
-            v-model="scope.row.authority.authorityId"
-            :options="authOptions"
-            :show-all-levels="false"
-            :props="{ checkStrictly: true,label:'authorityName',value:'authorityId',disabled:'disabled',emitPath:false}"
-            filterable
+              @change="changeAuthority(scope.row)"
+              v-model="scope.row.authority.authorityId"
+              :options="authOptions"
+              :show-all-levels="false"
+              :props="{ checkStrictly: true,label:'authorityName',value:'authorityId',disabled:'disabled',emitPath:false}"
+              filterable
           ></el-cascader>
         </template>
       </el-table-column>
@@ -40,14 +40,14 @@
       </el-table-column>
     </el-table>
     <el-pagination
-      :current-page="page"
-      :page-size="pageSize"
-      :page-sizes="[10, 30, 50, 100]"
-      :style="{float:'right',padding:'20px'}"
-      :total="total"
-      @current-change="handleCurrentChange"
-      @size-change="handleSizeChange"
-      layout="total, sizes, prev, pager, next, jumper"
+        :current-page="page"
+        :page-size="pageSize"
+        :page-sizes="[10, 30, 50, 100]"
+        :style="{float:'right',padding:'20px'}"
+        :total="total"
+        @current-change="handleCurrentChange"
+        @size-change="handleSizeChange"
+        layout="total, sizes, prev, pager, next, jumper"
     ></el-pagination>
 
     <el-dialog :visible.sync="addUserDialog" custom-class="user-dialog" title="新增用户">
@@ -63,18 +63,18 @@
         </el-form-item>
         <el-form-item label="头像" label-width="80px">
           <div style="display:inline-block" @click="openHeaderChange">
-            <img class="header-img-box" v-if="userInfo.headerImg" :src="userInfo.headerImg" />
+            <img class="header-img-box" v-if="userInfo.headerImg" :src="path+userInfo.headerImg"/>
             <div v-else class="header-img-box">从媒体库选择</div>
           </div>
         </el-form-item>
         <el-form-item label="用户角色" label-width="80px" prop="authorityId">
           <el-cascader
-            @change="changeAuthority(scope.row)"
-            v-model="userInfo.authorityId"
-            :options="authOptions"
-            :show-all-levels="false"
-            :props="{ checkStrictly: true,label:'authorityName',value:'authorityId',disabled:'disabled',emitPath:false}"
-            filterable
+              @change="changeAuthority(scope.row)"
+              v-model="userInfo.authorityId"
+              :options="authOptions"
+              :show-all-levels="false"
+              :props="{ checkStrictly: true,label:'authorityName',value:'authorityId',disabled:'disabled',emitPath:false}"
+              filterable
           ></el-cascader>
         </el-form-item>
       </el-form>
@@ -97,15 +97,16 @@ import {
   register,
   deleteUser
 } from "@/api/user";
-import { getAuthorityList } from "@/api/authority";
+import {getAuthorityList} from "@/api/authority";
 import infoList from "@/mixins/infoList";
-import { mapGetters } from "vuex";
+import {mapGetters} from "vuex";
 import CustomPic from "@/components/customPic";
 import ChooseImg from "@/components/chooseImg";
+
 export default {
   name: "Api",
   mixins: [infoList],
-  components: { CustomPic,ChooseImg },
+  components: {CustomPic, ChooseImg},
   data() {
     return {
       listApi: getUserList,
@@ -121,18 +122,18 @@ export default {
       },
       rules: {
         username: [
-          { required: true, message: "请输入用户名", trigger: "blur" },
-          { min: 6, message: "最低6位字符", trigger: "blur" }
+          {required: true, message: "请输入用户名", trigger: "blur"},
+          {min: 6, message: "最低6位字符", trigger: "blur"}
         ],
         password: [
-          { required: true, message: "请输入用户密码", trigger: "blur" },
-          { min: 6, message: "最低6位字符", trigger: "blur" }
+          {required: true, message: "请输入用户密码", trigger: "blur"},
+          {min: 6, message: "最低6位字符", trigger: "blur"}
         ],
         nickName: [
-          { required: true, message: "请输入用户昵称", trigger: "blur" }
+          {required: true, message: "请输入用户昵称", trigger: "blur"}
         ],
         authorityId: [
-          { required: true, message: "请选择用户角色", trigger: "blur" }
+          {required: true, message: "请选择用户角色", trigger: "blur"}
         ]
       }
     };
@@ -141,7 +142,7 @@ export default {
     ...mapGetters("user", ["token"])
   },
   methods: {
-    openHeaderChange(){
+    openHeaderChange() {
       this.$refs.chooseImg.open()
     },
     setOptions(authData) {
@@ -150,26 +151,26 @@ export default {
     },
     setAuthorityOptions(AuthorityData, optionsData) {
       AuthorityData &&
-        AuthorityData.map(item => {
-          if (item.children && item.children.length) {
-            const option = {
-              authorityId: item.authorityId,
-              authorityName: item.authorityName,
-              children: []
-            };
-            this.setAuthorityOptions(item.children, option.children);
-            optionsData.push(option);
-          } else {
-            const option = {
-              authorityId: item.authorityId,
-              authorityName: item.authorityName
-            };
-            optionsData.push(option);
-          }
-        });
+      AuthorityData.map(item => {
+        if (item.children && item.children.length) {
+          const option = {
+            authorityId: item.authorityId,
+            authorityName: item.authorityName,
+            children: []
+          };
+          this.setAuthorityOptions(item.children, option.children);
+          optionsData.push(option);
+        } else {
+          const option = {
+            authorityId: item.authorityId,
+            authorityName: item.authorityName
+          };
+          optionsData.push(option);
+        }
+      });
     },
     async deleteUser(row) {
-      const res = await deleteUser({ id: row.ID });
+      const res = await deleteUser({id: row.ID});
       if (res.code == 0) {
         this.getTableData();
         row.visible = false;
@@ -180,7 +181,7 @@ export default {
         if (valid) {
           const res = await register(this.userInfo);
           if (res.code == 0) {
-            this.$message({ type: "success", message: "创建成功" });
+            this.$message({type: "success", message: "创建成功"});
           }
           await this.getTableData();
           this.closeAddUserDialog();
@@ -203,13 +204,13 @@ export default {
         authorityId: row.authority.authorityId
       });
       if (res.code == 0) {
-        this.$message({ type: "success", message: "角色设置成功" });
+        this.$message({type: "success", message: "角色设置成功"});
       }
     }
   },
   async created() {
     this.getTableData();
-    const res = await getAuthorityList({ page: 1, pageSize: 999 });
+    const res = await getAuthorityList({page: 1, pageSize: 999});
     this.setOptions(res.data.list);
   }
 };
@@ -218,6 +219,7 @@ export default {
 
 .button-box {
   padding: 10px 20px;
+
   .el-button {
     float: right;
   }
@@ -225,17 +227,19 @@ export default {
 
 .user-dialog {
   .header-img-box {
-  width: 200px;
-  height: 200px;
-  border: 1px dashed #ccc;
-  border-radius: 20px;
-  text-align: center;
-  line-height: 200px;
-  cursor: pointer;
-}
+    width: 200px;
+    height: 200px;
+    border: 1px dashed #ccc;
+    border-radius: 20px;
+    text-align: center;
+    line-height: 200px;
+    cursor: pointer;
+  }
+
   .avatar-uploader .el-upload:hover {
     border-color: #409eff;
   }
+
   .avatar-uploader-icon {
     border: 1px dashed #d9d9d9 !important;
     border-radius: 6px;
@@ -246,6 +250,7 @@ export default {
     line-height: 178px;
     text-align: center;
   }
+
   .avatar {
     width: 178px;
     height: 178px;
